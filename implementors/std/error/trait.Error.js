b42262eab7461a45b558a974be6cb03f1252e8c0
Travis CI User <travis@example.org>
2019-09-06 17:33:23 +0000

    Documentation for rust-random/rand@9a4a800

---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,12 @@
+(function() {var implementors = {};
+implementors["rand"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"rand/distributions/weighted/enum.WeightedError.html\" title=\"enum rand::distributions::weighted::WeightedError\">WeightedError</a>",synthetic:false,types:["rand::distributions::weighted::WeightedError"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"rand/rngs/adapter/struct.ReadError.html\" title=\"struct rand::rngs::adapter::ReadError\">ReadError</a>",synthetic:false,types:["rand::rngs::adapter::read::ReadError"]},];
+implementors["rand_core"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"rand_core/struct.Error.html\" title=\"struct rand_core::Error\">Error</a>",synthetic:false,types:["rand_core::error::Error"]},];
+implementors["rand_jitter"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"rand_jitter/enum.TimerError.html\" title=\"enum rand_jitter::TimerError\">TimerError</a>",synthetic:false,types:["rand_jitter::error::TimerError"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ source-files.js
@@ -0,0 +1,14 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["rand"] = {"name":"","dirs":[{"name":"distributions","dirs":[{"name":"weighted","files":["alias_method.rs","mod.rs"]}],"files":["bernoulli.rs","binomial.rs","cauchy.rs","dirichlet.rs","exponential.rs","float.rs","gamma.rs","integer.rs","mod.rs","normal.rs","other.rs","pareto.rs","poisson.rs","triangular.rs","uniform.rs","unit_circle.rs","unit_sphere.rs","utils.rs","weibull.rs","ziggurat_tables.rs"]},{"name":"rngs","dirs":[{"name":"adapter","files":["mod.rs","read.rs","reseeding.rs"]}],"files":["entropy.rs","mock.rs","mod.rs","small.rs","std.rs","thread.rs"]},{"name":"seq","files":["index.rs","mod.rs"]}],"files":["lib.rs","prelude.rs"]};
+sourcesIndex["rand_chacha"] = {"name":"","files":["chacha.rs","lib.rs"]};
+sourcesIndex["rand_core"] = {"name":"","files":["block.rs","error.rs","impls.rs","le.rs","lib.rs","os.rs"]};
+sourcesIndex["rand_distr"] = {"name":"","files":["binomial.rs","cauchy.rs","dirichlet.rs","exponential.rs","gamma.rs","lib.rs","normal.rs","pareto.rs","pert.rs","poisson.rs","triangular.rs","unit_ball.rs","unit_circle.rs","unit_disc.rs","unit_sphere.rs","utils.rs","weibull.rs","ziggurat_tables.rs"]};
+sourcesIndex["rand_hc"] = {"name":"","files":["hc128.rs","lib.rs"]};
+sourcesIndex["rand_isaac"] = {"name":"","files":["isaac.rs","isaac64.rs","isaac_array.rs","lib.rs"]};
+sourcesIndex["rand_jitter"] = {"name":"","files":["error.rs","lib.rs","platform.rs"]};
+sourcesIndex["rand_os"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["rand_pcg"] = {"name":"","files":["lib.rs","pcg128.rs","pcg64.rs"]};
+sourcesIndex["rand_wasm_bindgen_test"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["rand_xorshift"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["rand_xoshiro"] = {"name":"","files":["common.rs","lib.rs","splitmix64.rs","xoroshiro128plus.rs","xoroshiro128plusplus.rs","xoroshiro128starstar.rs","xoroshiro64star.rs","xoroshiro64starstar.rs","xoshiro128plus.rs","xoshiro128plusplus.rs","xoshiro128starstar.rs","xoshiro256plus.rs","xoshiro256plusplus.rs","xoshiro256starstar.rs","xoshiro512plus.rs","xoshiro512plusplus.rs","xoshiro512starstar.rs"]};
+createSourceSidebar();
